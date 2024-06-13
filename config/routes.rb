20704fbb9e1039f3791dc8da7318b08b7f4e8bcf
thaniaputri02 <thaniaputriaida@gmail.com>
2024-06-13 13:17:58 +0000

    Selesai 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :articles
   resources :users
+
+  root to:'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
